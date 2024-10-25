dd9dc20f04a70eb446632ede712652abb5b4dc03
Segun Ade <giftsegunade@gmail.com>
2024-10-25 18:30:08 +0100

    update code to log that the task is added when function call is successful

--- cmd/addTask.go
+++ cmd/addTask.go
@@ -1,6 +1,10 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var addTaskCmd = &cobra.Command{
 	Use: "Add Task",
@@ -9,7 +13,8 @@ var addTaskCmd = &cobra.Command{
 	Long: "Add tasks to todo list",
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		AddTask(args[0])
+	fmt.Println(AddTask(args[0]))	
+	fmt.Println("Task added")
 	},
 }
 

c62d9ea120734216bac456f5afa1960d077dc654
Segun Ade <giftsegunade@gmail.com>
2024-10-25 18:32:39 +0100

    update code to append task to csv file

--- cmd/todo.go
+++ cmd/todo.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -9,36 +8,62 @@ import (
 )
 
 type Todo struct {
-	ID      int  `csv:"id"`
-	Task    interface{}  `csv:"task"`
-	Created time.Time  `csv:"created"`
+	ID      int    `csv:"id"`
+	Task    string `csv:"task"`
+	Done bool `csv:"done"`
+	Created string `csv:"created"`
 }
 
-func AddTask(task interface{}) error{
+func AddTask(task string) error {
 	file, err := os.OpenFile("task.csv", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	
+
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error while executing %s/n", err)
 		return err
 	}
 	defer file.Close()
 
-	fileInfo, err := file.Stat()
+	existingFile, err := os.Open("task.csv")
+	if err != nil {
+		return err
+	}
+	defer existingFile.Close()
+
+	fileInfo, err := existingFile.Stat()
 	if err != nil {
 		return err
 	}
 
-	tasks := []*Todo{
-		{1, task, time.Now()},
+	var tasks []*Todo
+
+	if fileInfo.Size() > 0 {
+
+		if err := gocsv.UnmarshalFile(existingFile, &tasks); err != nil {
+			return err
+		}
+	}
+	var newID int
+	if len(tasks) > 0 {
+		lastTask := tasks[len(tasks)-1]
+		newID = lastTask.ID + 1
+	} else {
+		newID = 1
+	}
+
+	newTask := &Todo{
+		ID:      newID,
+		Task:    task,
+		Done: false,
+		Created: time.Now().Format("02 Jan 2006, 3:04 PM"),
 	}
 
 	if fileInfo.Size() == 0 {
-		err = gocsv.MarshalFile(tasks, file)
-		} else {
-		err = gocsv.MarshalWithoutHeaders(tasks, file)
+		err = gocsv.MarshalFile([]*Todo{newTask}, file)
+	} else {
+		err = gocsv.MarshalWithoutHeaders([]*Todo{newTask}, file)
 	}
+
 	if err != nil {
 		return err
 	}
-return nil
+	return nil
 }

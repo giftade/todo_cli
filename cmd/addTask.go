d887153672d8e12a32cdf74edb1b9e4e57127440
Segun Ade <giftsegunade@gmail.com>
2024-10-26 18:42:07 +0100

    update code to print out id of task when task is added

--- cmd/addTask.go
+++ cmd/addTask.go
@@ -7,14 +7,14 @@ import (
 )
 
 var addTaskCmd = &cobra.Command{
-	Use: "Add Task",
+	Use:     "Add Task",
 	Aliases: []string{"add"},
-	Short: "Add tasks to todo list",
-	Long: "Add tasks to todo list",
-	Args: cobra.ExactArgs(1),
+	Short:   "Add tasks to todo list",
+	Long:    "Add tasks to todo list",
+	Args:    cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-	fmt.Println(AddTask(args[0]))	
-	fmt.Println("Task added")
+		id, _ := AddTask(args[0])
+		fmt.Printf("Task added with ID: %d", id)
 	},
 }
 
